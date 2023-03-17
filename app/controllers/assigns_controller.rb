c7f078d6ef98f614877e7b92f70ed682fc236c16
kasumi sogabe <ksm17sgb@gmail.com>
2023-03-17 11:05:33 +0900

    Teamに所属しているUserの削除（離脱）は、そのTeamのオーナーか、そのUser自身しかできないようにする実装

--- app/controllers/assigns_controller.rb
+++ app/controllers/assigns_controller.rb
@@ -16,9 +16,14 @@ class AssignsController < ApplicationController
 
   def destroy
     assign = Assign.find(params[:id])
-    destroy_message = assign_destroy(assign, assign.user)
-
-    redirect_to team_url(params[:team_id]), notice: destroy_message
+    assigned_user = assign.user
+    # チームのオーナーまたは削除対象のユーザー本人のみ削除できるようにする
+    unless assign.team.owner == current_user || assigned_user == current_user
+      redirect_to team_url(params[:team_id]), notice: I18n.t('views.messages.cannot_delete_member')
+    return
+  end
+  destroy_message = assign_destroy(assign, assigned_user)
+  redirect_to team_url(params[:team_id]), notice: destroy_message
   end
 
   private
